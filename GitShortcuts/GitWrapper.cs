b835d1a1a47019fb2802896d738a82b9c621523e
SebastianNiebl <github@DerSaege.e4ward.com>
2022-06-17 19:26:35 +0200

    First Test

--- GitShortcuts/GitWrapper.cs
+++ GitShortcuts/GitWrapper.cs
@@ -17,13 +17,37 @@ namespace GitShortcuts
             proc.StartInfo.FileName = "git";
             proc.StartInfo.Arguments = "status";
             proc.StartInfo.RedirectStandardOutput = true;
-            proc.OutputDataReceived += new DataReceivedEventHandler((sender, e) => { sOut += e.Data; });
+            proc.OutputDataReceived += new DataReceivedEventHandler((sender, e) => { sOut += e.Data + "\r\n"; });
             proc.Start();
             proc.BeginOutputReadLine();
-            proc.WaitForExit();// Waits here for the process to exit.
-            string line1 = sOut.Split('\n')[0];
-            return line1;
-        }
+            proc.WaitForExit();
 
+            string line1 = sOut.Split("\r\n")[0];
+            return line1.Substring(10);
+        }
+        public void Checkout(string branch)
+        {
+            Process proc = new();
+            string sOut = "";
+            proc.StartInfo.FileName = "git";
+            proc.StartInfo.Arguments = string.Format("checkout {0}", branch);
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.OutputDataReceived += new DataReceivedEventHandler((sender, e) => { sOut += e.Data + "\r\n"; });
+            proc.Start();
+            proc.BeginOutputReadLine();
+            proc.WaitForExit();
+        }
+        public void Merge(string branch)
+        {
+            Process proc = new();
+            string sOut = "";
+            proc.StartInfo.FileName = "git";
+            proc.StartInfo.Arguments = string.Format("merge {0} -X theirs", branch);
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.OutputDataReceived += new DataReceivedEventHandler((sender, e) => { sOut += e.Data + "\r\n"; });
+            proc.Start();
+            proc.BeginOutputReadLine();
+            proc.WaitForExit();
+        }
     }
 }
