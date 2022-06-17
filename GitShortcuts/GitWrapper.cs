ed5c56e1b8ac0c2993992d20182f6983f661f963
SebastianNiebl <github@DerSaege.e4ward.com>
2022-06-17 19:02:56 +0200

    Initial commit

---
+++ GitShortcuts/GitWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace GitShortcuts
+{
+    class GitWrapper
+    {
+
+        public string GetCurrentBranch()
+        {
+            Process proc = new();
+            string sOut = "";
+            proc.StartInfo.FileName = "git";
+            proc.StartInfo.Arguments = "status";
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.OutputDataReceived += new DataReceivedEventHandler((sender, e) => { sOut += e.Data; });
+            proc.Start();
+            proc.BeginOutputReadLine();
+            proc.WaitForExit();// Waits here for the process to exit.
+            string line1 = sOut.Split('\n')[0];
+            return line1;
+        }
+
+    }
+}
---
+++ GitShortcuts/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitShortcuts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GitWrapper g = new();
+            Console.WriteLine(g.GetCurrentBranch());
+        }
+    }
+}
